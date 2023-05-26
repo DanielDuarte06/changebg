1cc5dff3677e78f0a1539bc1decb2e567c7bc051
VITOR SANTANA DE JESUS <sn1079379@cfp127aluno.local>
2023-05-26 11:50:39 -0300

    melhorias

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
-
 function Color() {
- 
-   if (document.body.style.backgroundColor == "black") {
-    document.body.style.backgroundColor = 'blue';
-   }
-   else {
-    document.body.style.backgroundColor = 'black';
-     console.log("mudou para black");
-   }
+
+  var changeColor = document.querySelector('body');
+  //console.log(changeColor);
+
+  switch (changeColor.className) {
+    case "body_red":
+      changeColor.classList.remove("body_red");
+      changeColor.classList.add("body_black");
+      break;
+
+    case "body_black":
+      changeColor.classList.remove("body_black");
+      changeColor.classList.add("body_blue");
+      break;
+
+    case "body_blue":
+      changeColor.classList.remove("body_blue");
+      changeColor.classList.add("body_red");
+      break;
+
+    default:
+      console.log("Falhou !!");
+  }
 }
 
 window.onclick = () => {
